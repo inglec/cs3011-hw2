f37cbcc043d735d572fa5dcd6a24b65e6755d14b
inglec <inglec@tcd.ie>
2017-12-01 20:30:13 +0000

    made insignificant changes

--- q3.pl
+++ q3.pl
@@ -1,3 +1,6 @@
+% Ciarán Ingle
+%   15319981
+%
 % Q3)
 %   Write a DCG that given a non-negative integer Sum, accepts lists of integers >= 1 that add up to sum.
 %
@@ -13,4 +16,4 @@ mkList(0, []).
 mkList(N, [N|T]) :- N > 0, X is N-1, mkList(X, T).
 
 s(0) --> [].
-s(Sum) --> {mkList(Sum, DescList), member(Item, DescList), Remainder is Sum - Item}, [Item], s(Remainder).
+s(N) --> {mkList(N, DescList), member(Item, DescList), Remainder is N-Item}, [Item], s(Remainder).
