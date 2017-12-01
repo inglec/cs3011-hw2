3892f0d06179cf59bb03dd792158bdca0f14e486
inglec <inglec@tcd.ie>
2017-12-01 19:25:27 +0000

    Q1 complete

--- q1.pl
+++ q1.pl
@@ -1,3 +1,6 @@
+% Ciarán Ingle
+%   15319981
+%
 % Q1)
 %   Write a DCG that accepts strings of the form u2v where:
 %   - u, v are strings of the alphabet {0, 1}
@@ -9,4 +12,14 @@
 %   false.
 %
 
-s([2], []). % no 1s and no 0s
+s --> u(Count), two, v(Count).
+
+u(0)     --> [].
+u(Count) --> [0], u(Decrement), {Count is Decrement+1}.
+u(Count) --> [1], u(Count).
+
+two --> [2].
+
+v(0)     --> [].
+v(Count) --> [1], v(Decrement), {Count is Decrement+1}.
+v(Count) --> [0], v(Count).
